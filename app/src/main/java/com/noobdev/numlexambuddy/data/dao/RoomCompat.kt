5e9b4a8c48fc64aabfe2686829f71d0e9bb93cce
Umais-Adeed <umaisadeed910@gmail.com>
2025-06-02 09:51:50 +0500

    Refactor RoomCompat annotation to use single SuppressWarnings
    The RoomCompat annotation was previously using both `@SuppressWarnings`
    and `@androidx.room.RoomWarnings` to suppress the same warnings.
    This change consolidates this into a single `@SuppressWarnings`
    annotation.

--- app/src/main/java/com/noobdev/numlexambuddy/data/dao/RoomCompat.kt
+++ app/src/main/java/com/noobdev/numlexambuddy/data/dao/RoomCompat.kt
@@ -6,8 +6,4 @@ import androidx.room.RoomWarnings
  * Common Room annotations to fix signature issues in DAO interfaces
  */
 @SuppressWarnings(RoomWarnings.CURSOR_MISMATCH, RoomWarnings.MISSING_JAVA_TMP_DIR)
-@androidx.room.RoomWarnings(
-    RoomWarnings.CURSOR_MISMATCH,
-    RoomWarnings.MISSING_JAVA_TMP_DIR
-)
 annotation class RoomCompat
